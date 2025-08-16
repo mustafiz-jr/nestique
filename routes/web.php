e0bdd51ba26325f9eb4c65f9e0caa9fd47a2c339
Mustafizur <mustafijjr@gmail.com>
2025-08-16 22:56:29 +0600

    link

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::get('contact', [PageController::class, 'contact'])->name('contact');
 Route::get('about', [PageController::class, 'about'])->name('about');
 Route::get('cart', [PageController::class, 'cart'])->name('cart');
 Route::get('checkout', [PageController::class, 'checkout'])->name('checkout');
-Route::get('/cart',[CartController::class , 'add_cart'])->name('cart.add');
+// Route::get('/cart',[CartController::class , 'add_cart'])->name('cart.add');
 
 // Route::get('ajax/get_cart_count', [AjaxController::class, 'get_cart_count'])->name('get_cart_count');
