1c2b80f254193d101318fd0f07060061fa7de6ba
Mustafizur <mustafijjr@gmail.com>
2025-08-16 20:31:53 +0600

    humm

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    //
+    public function cart_add()
+    {
+        Cart::add([
+            ['id' => '1', 'name' => 'Product 1', 'qty' => 1, 'price' => 10.00, 'weight' => 550],
+            ['id' => '2', 'name' => 'Product 2', 'qty' => 1, 'price' => 10.00, 'weight' => 550, 'options' => ['size' => 'large']]
+        ]);
+
+        // Cart::add([$product1, $product2]);
+    }
 }
