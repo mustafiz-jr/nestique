296739bad5856bbda3d1342422e1e1b46e2f62d8
Mustafizur <mustafijjr@gmail.com>
2025-08-18 01:26:24 +0600

    kn

--- config/app.php
+++ config/app.php
@@ -122,20 +122,4 @@ return [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
         'store' => env('APP_MAINTENANCE_STORE', 'database'),
     ],
-
-    'providers' => [
-        /*
-     * Laravel Framework Service Providers...
-     */
-        Illuminate\Auth\AuthServiceProvider::class,
-        // ... অন্যান্য সার্ভিস প্রোভাইডার
-
-        /*
-     * Application Service Providers...
-     */
-        App\Providers\AppServiceProvider::class,
-        // ...
-        App\Providers\CustomCssServiceProvider::class, // আপনার নতুন সার্ভিস প্রোভাইডার এখানে যোগ করুন
-    ],
-
 ];
